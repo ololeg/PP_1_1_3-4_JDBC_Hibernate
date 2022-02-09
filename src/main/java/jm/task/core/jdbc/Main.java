582ee5378197a06f4f535e99cdf8c119aa9cd7f8
ololeg <kalabin-o@mail.ru>
2022-02-09 15:03:01 +0500

    1.1.3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,20 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws SQLException {
+        UserService one = new UserServiceImpl();
+        one.createUsersTable();
+        one.saveUser("Иван", "Иванов", (byte) 33);
+        one.saveUser("Наташа", "Ростова", (byte) 22);
+        one.saveUser("Никита", "Ларин", (byte) 17);
+        one.saveUser("Андрей", "Васильев", (byte) 55);
+        one.getAllUsers().forEach(System.out::println);
+        one.cleanUsersTable();
+        one.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,34 @@
 package jm.task.core.jdbc.util;
 
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+
+    java.sql.Connection connection = null;
+
+    public Util() {
+        try {
+            Driver driver = new com.mysql.cj.jdbc.Driver();
+            DriverManager.registerDriver(driver);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        try {
+            String URL = "jdbc:mysql://localhost:3306/newBd";
+            String USERNAME = "root";
+            String PASSWORD = "root";
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Driver not found");
+        }
+        return connection;
+    }
 }
